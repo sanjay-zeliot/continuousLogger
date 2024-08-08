d14b5203334cc79408a6b15576f47b94bd678235
sanjay-zeliot <sanjay@zeliot.in>
2024-08-08 18:27:13 +0530

    Added Interval in the ENV

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
+let INTERVAL = parseInt(process.env.INTERVAL || 1);
+
 const startLogging = () => {
-  let i = 1;
+  let i = INTERVAL;
   setInterval(() => {
-    console.log("Hello" + i++);
-  }, 2000);
+    console.log("Hello" + i);
+    i += INTERVAL;
+  }, INTERVAL * 1000);
 };
 
 startLogging();
