4b7900a3e9507a77beb7ad95f46e3a0fb9ce47b1
sanjay-zeliot <sanjay-zeliot@example.com>
2024-08-18 06:43:07 +0000

    testcommit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const startLogging = () => {
   let i = 1;
   setInterval(() => {
     console.log("Hello World!!" + i++);
+    console.log("Hello from Oman for Teltonika devices!!");
   }, 2000);
 };
 
