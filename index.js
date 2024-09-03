f70733ee0ae4d09c2389079d8dff9404e683b932
sanjay-zeliot <sanjay-zeliot@example.com>
2024-09-03 13:33:57 +0000

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const startLogging = () => {
   let i = 1;
   setInterval(() => {
     console.log("Hello World!!" + i++);
-    console.log("Hello from Oman for Teltonika devices!!");
+    console.log("Hello from Oman for Teltonika devices!!!!");
   }, 2000);
 };
 
