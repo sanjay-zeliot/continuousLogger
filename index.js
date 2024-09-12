b0baf7e3696db67349f9fef0b2b7db9560e15d3c
sanjay-zeliot <sanjay-zeliot@example.com>
2024-09-12 21:49:42 +0000

    test123456

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const startLogging = () => {
   let i = 1;
   setInterval(() => {
     console.log("Hello World!!" + i++);
-    console.log("Hi John this is Cndense!!!!");
+    console.log("Hi this is Condense!!!!");
   }, 2000);
 };
 
